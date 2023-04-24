5e47f70c95698ffbef9d4a290decb7e1987d58f9
Co0d <papka_jove_2004@mail.ru>
2023-04-24 12:27:34 +0300

    Add FilePractik

---
+++ Practica/AMONIC/Model/AppData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMONIC.Model
+{
+    internal class AppData
+    {
+        public static Session1_XXEntities2 db = new Session1_XXEntities2();
+        public static Session2_XXEntities db1 = new Session2_XXEntities();
+    }
+}
